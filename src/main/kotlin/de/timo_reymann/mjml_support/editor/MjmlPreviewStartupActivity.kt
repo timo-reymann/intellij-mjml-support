c419eac7856a74ff1f2103ef1c09c52b2d482668
Timo Reymann <Timo.Reymann@trustedshops.de>
2021-04-08 12:25:00 +0200

    feat: Load preview files on startup of the ide

---
+++ src/main/kotlin/de/timo_reymann/mjml_support/editor/MjmlPreviewStartupActivity.kt
@@ -0,0 +1,19 @@
+package de.timo_reymann.mjml_support.editor
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.StartupActivity
+import com.intellij.util.io.Decompressor
+import de.timo_reymann.mjml_support.util.FilePluginUtil
+
+/**
+ * Activity to copy editor files over
+ */
+class MjmlPreviewStartupActivity : StartupActivity {
+    override fun runActivity(project: Project) {
+        val rendererZip = FilePluginUtil.getFile(MjmlJCEFHtmlPanel.RENDERER_ARCHIVE_NAME)
+        FilePluginUtil.copyFile("node", MjmlJCEFHtmlPanel.RENDERER_ARCHIVE_NAME)
+        Decompressor.Zip(rendererZip)
+            .extract(FilePluginUtil.getFile("renderer"))
+        rendererZip.delete()
+    }
+}
