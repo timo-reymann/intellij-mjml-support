21a141ef8bacf7a4769a1f978d0aa27372773cfe
Timo Reymann <mail@timo-reymann.de>
2021-07-03 13:09:17 +0200

    fix(#34): Add file lock to prevent multiple running intellij instances to run in concurrency problems for builtin rendering script

--- src/main/kotlin/de/timo_reymann/mjml_support/editor/render/BuiltinRenderResourceProvider.kt
+++ src/main/kotlin/de/timo_reymann/mjml_support/editor/render/BuiltinRenderResourceProvider.kt
@@ -1,12 +1,16 @@
 package de.timo_reymann.mjml_support.editor.render
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.intellij.notification.NotificationType
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.util.io.Decompressor
 import de.timo_reymann.mjml_support.editor.MjmlPreviewStartupActivity
 import de.timo_reymann.mjml_support.util.FilePluginUtil
 import de.timo_reymann.mjml_support.editor.MjmlJCEFHtmlPanel
+import de.timo_reymann.mjml_support.util.FileLockFailedException
+import de.timo_reymann.mjml_support.util.FileLockUtil
+import de.timo_reymann.mjml_support.util.MessageBusUtil
 
 object BuiltinRenderResourceProvider {
     private var mjmlVersion: String = "?"
@@ -19,27 +23,50 @@ object BuiltinRenderResourceProvider {
 
     fun copyResources() {
         val rendererZip = FilePluginUtil.getFile(MjmlJCEFHtmlPanel.RENDERER_ARCHIVE_NAME)
-        FilePluginUtil.copyFile("node", MjmlJCEFHtmlPanel.RENDERER_ARCHIVE_NAME)
-        Decompressor.Zip(rendererZip)
-            .extract(FilePluginUtil.getFile("renderer"))
+        val lockFile = FilePluginUtil.getFile(MjmlJCEFHtmlPanel.RENDERER_ARCHIVE_NAME + ".lock")
 
+        try {
+            FileLockUtil.runWithLock(lockFile) {
+                FilePluginUtil.copyFile("node", MjmlJCEFHtmlPanel.RENDERER_ARCHIVE_NAME)
+                Decompressor.Zip(rendererZip)
+                    .extract(FilePluginUtil.getFile("renderer"))
+
+                extractMjmlVersion()
+
+                try {
+                    rendererZip.delete()
+                } catch (e: Exception) {
+                    logger.warn("Failed to delete render zip", e)
+                }
+
+                notifyRendererResourcesChanged()
+            }
+
+        } catch (e: FileLockFailedException) {
+            logger.warn("Failed to get file lock to copy renderer resources", e)
+            MessageBusUtil.showMessage(
+                NotificationType.WARNING,
+                "Failed to copy resources for builtin mjml rendering",
+                "This may be because another instance is already copying the files.<br/>" +
+                        "You can also retry it manually under <b>Settings > Tools > MJML-Settings</b> | 'Copy renderer files for mjml preview'"
+            )
+        }
+    }
+
+    private fun notifyRendererResourcesChanged() {
+        ApplicationManager.getApplication()
+            .messageBus
+            .syncPublisher(MJML_PREVIEW_FORCE_RENDER_TOPIC)
+            .onForcedRender()
+    }
+
+    private fun extractMjmlVersion() {
         try {
             mjmlVersion = parseMjmlVersion()
         } catch (e: Exception) {
             mjmlVersion = "N/A"
             logger.warn("Failed to parse mjml version", e)
         }
-
-        try {
-            rendererZip.delete()
-        } catch (e: Exception) {
-            logger.warn("Failed to delete render zip", e)
-        }
-
-        ApplicationManager.getApplication()
-            .messageBus
-            .syncPublisher(MJML_PREVIEW_FORCE_RENDER_TOPIC)
-            .onForcedRender()
     }
 
     private fun parseMjmlVersion(): String {
