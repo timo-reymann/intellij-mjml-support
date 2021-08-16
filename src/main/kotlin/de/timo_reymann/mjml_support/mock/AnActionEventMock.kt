c5fc94958924f31a3e9e787d981130d1bd5dd53b
Timo Reymann <Timo.Reymann@trustedshops.de>
2021-08-16 13:24:38 +0200

    chore: Fix deprecated api usage + since build

--- src/main/kotlin/de/timo_reymann/mjml_support/mock/AnActionEventMock.kt
+++ src/main/kotlin/de/timo_reymann/mjml_support/mock/AnActionEventMock.kt
@@ -3,15 +3,24 @@ package de.timo_reymann.mjml_support.mock
 import com.intellij.ide.DataManager
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.actionSystem.Presentation
+import org.jetbrains.concurrency.await
+import javax.swing.JComponent
 
 /**
  * Create an empty AnActionEvent
  */
-class AnActionEventMock : AnActionEvent( null,
-    DataManager.getInstance().dataContext,
+class AnActionEventMock() : AnActionEvent( null,
+     DummyDataContext(),
     "",
     Presentation(),
     ActionManager.getInstance(),
     0) {
+}
+
+internal class DummyDataContext : DataContext {
+    override fun getData(dataId: String): Any? {
+        return null
+    }
 }
