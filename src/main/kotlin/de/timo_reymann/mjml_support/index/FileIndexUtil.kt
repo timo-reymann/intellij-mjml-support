2c21528cd68acdc23b90ceb3fc477a97e01f293d
Timo Reymann <mail@timo-reymann.de>
2022-04-22 13:35:14 +0200

    feat(#120): Add support for template languages in includes (#121)
    * fix(#120): Allow template files with data language type mjml in includes
    
    * chore: Refactor to method expressions
    
    * feat(#120): Add draft for mjml template data language in file index autocompletion
    
    * feat(#120): Add changelog notes for 2.2.0

--- src/main/kotlin/de/timo_reymann/mjml_support/index/FileIndexUtil.kt
+++ src/main/kotlin/de/timo_reymann/mjml_support/index/FileIndexUtil.kt
@@ -2,53 +2,62 @@ package de.timo_reymann.mjml_support.index
 
 import com.intellij.navigation.ChooseByNameContributor
 import com.intellij.navigation.ChooseByNameContributorEx
+import com.intellij.openapi.fileTypes.FileNameMatcher
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.fileTypes.FileTypeManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.io.FileUtilRt
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiFile
 import com.intellij.psi.search.FilenameIndex
+import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.search.ProjectScope
 import com.intellij.util.Processor
 import com.intellij.util.containers.ContainerUtil
 import com.intellij.util.indexing.FindSymbolParameters
+import com.intellij.webcore.template.TemplateLanguageFileUtil
+import de.timo_reymann.mjml_support.lang.MjmlHtmlLanguage
 import java.io.File
 import java.util.*
 
 object FileIndexUtil {
-    fun getMatchesForAutoComplete(project: Project, rootFile: VirtualFile, fileTypes: Set<FileType>, invocationCount: Int): MutableSet<String> {
+    fun getMatchesForAutoComplete(
+        project: Project,
+        rootFile: VirtualFile,
+        fileTypes: Set<FileType>,
+        invocationCount: Int
+    ): MutableSet<String> {
         val resultNames: MutableSet<String> = TreeSet()
         val results: MutableSet<String> = mutableSetOf()
+        val scope = ProjectScope.getProjectScope(project)
+        val files = mutableListOf<PsiFile>()
+
+        val fileTypeMatcher = fileTypes.flatMap {
+            FileTypeManager.getInstance().getAssociations(it)
+        }
 
         processAllNames(project) { fileName: String ->
-            for(fileType in fileTypes) {
-                if (filenameMatchesPrefixOrType(fileType, fileName, invocationCount)) {
-                    resultNames.add(fileName)
-                }
-            }
+            files.addAll(
+                getFilesMatchingPrefixOrType(
+                    project,
+                    scope,
+                    fileTypeMatcher,
+                    fileName,
+                    invocationCount
+                )
+            )
             true
         }
 
-        val scope = ProjectScope.getProjectScope(project)
-
-        for (resultName in resultNames) {
-
-            val files = FilenameIndex.getFilesByName(project, resultName, scope)
-
-            if (files.isEmpty()) {
+        for (psiFile in files) {
+            // ProgressManager.checkCanceled()
+            val virtualFile: VirtualFile = psiFile.virtualFile ?: continue
+            if (virtualFile == rootFile) {
                 continue
             }
 
-            for (psiFile in files) {
-                // ProgressManager.checkCanceled()
-                val virtualFile: VirtualFile = psiFile.virtualFile ?: continue
-                if (virtualFile == rootFile) {
-                    continue
-                }
-
-                val filePath: String = File(virtualFile.path).relativeTo(File(rootFile.parent.path)).toString()
-                results.add(filePath)
-            }
+            val filePath: String = File(virtualFile.path).relativeTo(File(rootFile.parent.path)).toString()
+            results.add(filePath)
         }
 
         return results
@@ -62,25 +71,41 @@ object FileIndexUtil {
                 } else {
                     ContainerUtil.process(contributor.getNames(project, false), processor)
                 }
-            } catch (ex: Exception) {
+            } catch (_: Exception) {
             }
         }
     }
 
-    private fun filenameMatchesPrefixOrType(fileType: FileType, fileName: String, invocationCount: Int): Boolean {
-        if (invocationCount > 2) {
-            return true
+    private fun getFilesMatchingPrefixOrType(
+        project: Project,
+        scope: GlobalSearchScope,
+        fileTypeMatcher: List<FileNameMatcher>,
+        fileName: String,
+        invocationCount: Int
+    ): Array<PsiFile> {
+        return when {
+            invocationCount > 2 || isFileTypeMatch(fileName, fileTypeMatcher) -> {
+                FilenameIndex.getFilesByName(project, fileName, scope)
+            }
+            else -> {
+                FilenameIndex.getFilesByName(project, fileName, scope)
+                    .filter {
+                        TemplateLanguageFileUtil.getTemplateDataLanguage(
+                            project,
+                            it.virtualFile
+                        ) == MjmlHtmlLanguage.INSTANCE
+                    }.toTypedArray()
+            }
         }
+    }
 
+    private fun isFileTypeMatch(fileName: String, fileTypeMatcher: List<FileNameMatcher>): Boolean {
+        // Ignore files without extensions
         val extension = FileUtilRt.getExtension(fileName)
         if (extension.isEmpty()) {
             return false
         }
 
-        for (matcher in FileTypeManager.getInstance().getAssociations(fileType)) {
-            if (matcher.acceptsCharSequence(fileName)) return true
-        }
-
-        return false
+        return fileTypeMatcher.any { it.acceptsCharSequence(fileName) }
     }
 }
