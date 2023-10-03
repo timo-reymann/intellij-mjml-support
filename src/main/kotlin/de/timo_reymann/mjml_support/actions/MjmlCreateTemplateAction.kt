edf666cf26c5ba812e336b8a6a92e2328f6e642e
Timo Reymann <mail@timo-reymann.de>
2023-10-03 10:30:26 +0200

    feat: Add validator for "new mjml file" action

--- src/main/kotlin/de/timo_reymann/mjml_support/actions/MjmlCreateTemplateAction.kt
+++ src/main/kotlin/de/timo_reymann/mjml_support/actions/MjmlCreateTemplateAction.kt
@@ -5,10 +5,32 @@ import com.intellij.ide.actions.CreateFileFromTemplateDialog
 import com.intellij.ide.fileTemplates.DefaultCreateFromTemplateHandler
 import com.intellij.ide.fileTemplates.FileTemplate
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.InputValidatorEx
 import com.intellij.psi.PsiDirectory
 import de.timo_reymann.mjml_support.bundle.MjmlBundle
 import de.timo_reymann.mjml_support.icons.MjmlIcons
 import de.timo_reymann.mjml_support.lang.MjmlHtmlFileType
+import java.nio.file.Files
+import java.nio.file.Path
+
+class NewMjmlFileNameValidator(private val project: Project) : InputValidatorEx {
+
+    override fun getErrorText(inputString: String): String? {
+        if (inputString.trim().isEmpty()) {
+            return MjmlBundle.message("action.new.file.error.empty.name")
+        }
+
+        if (Files.exists(Path.of(project.basePath, "$inputString.mjml"))) {
+            return MjmlBundle.message("action.new.file.error.file.exists", inputString)
+        }
+
+        return null
+    }
+
+    override fun checkInput(inputString: String): Boolean = true
+
+    override fun canClose(inputString: String): Boolean = getErrorText(inputString) == null
+}
 
 class MjmlCreateTemplateAction : CreateFileFromTemplateAction(TEMPLATE_NAME, DESCRIPTION, MjmlIcons.COLORED) {
     companion object {
@@ -21,6 +43,7 @@ class MjmlCreateTemplateAction : CreateFileFromTemplateAction(TEMPLATE_NAME, DES
         builder
             .setTitle(MjmlBundle.message("create_action.new", NAME))
             .addKind(NAME, MjmlIcons.COLORED, TEMPLATE_NAME)
+            .setValidator(NewMjmlFileNameValidator(project))
     }
 
     override fun getActionName(directory: PsiDirectory?, newName: String, templateName: String?): String =
