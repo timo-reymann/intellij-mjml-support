b7ebc4dd1f23918943e5477faadcadc88d5a8965
Timo Reymann <timo.reymann@deepl.com>
2025-03-05 22:11:27 +0100

    fix: Remove deprecated AnAction constructor

--- src/main/kotlin/de/timo_reymann/mjml_support/mock/AnActionEventMock.kt
+++ src/main/kotlin/de/timo_reymann/mjml_support/mock/AnActionEventMock.kt
@@ -1,18 +1,19 @@
 package de.timo_reymann.mjml_support.mock
 
-import com.intellij.openapi.actionSystem.ActionManager
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.Presentation
+import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.actionSystem.impl.SimpleDataContext
 
 /**
  * Create an empty AnActionEvent
  */
-class AnActionEventMock() : AnActionEvent( null,
-     SimpleDataContext.builder().build(),
-    "",
-    Presentation(),
-    ActionManager.getInstance(),
-    0) {
+object AnActionEventMock {
+    fun create(): AnActionEvent {
+        return AnActionEvent.createEvent(
+            SimpleDataContext.builder().build(),
+            Presentation(),
+            "",
+            ActionUiKind.TOOLBAR,
+            null,
+        )
+    }
 }
-
